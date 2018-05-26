4151f6602c3b004b135cfc03433e7726b358b3c2
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2018-05-26 14:15:17 +0900

    CNNのクラスの実装まで

--- cnn/im2col_.py
+++ cnn/im2col_.py
@@ -24,18 +24,18 @@ def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
     col : 2次元配列
     """
     N, C, H, W = input_data.shape
-    print(N, C, H, W)
+    # print(N, C, H, W)
     out_h = (H + 2*pad - filter_h)//stride + 1
-    print('out_h: '+str(out_h))
+    # print('out_h: '+str(out_h))
     out_w = (W + 2*pad - filter_w)//stride + 1
-    print('out_w : '+str(out_w))
+    # print('out_w : '+str(out_w))
     
     # データが入っている
     img = np.pad(input_data, [(0,0), (0,0), (pad, pad), (pad, pad)], 'constant')
-    print('img.shape : '+str(img.shape))
+    # print('img.shape : '+str(img.shape))
     # 最終的に
     col = np.zeros((N, C, filter_h, filter_w, out_h, out_w))
-    print("col.shape : "+str(col.shape))
+    # print("col.shape : "+str(col.shape))
 
     for y in range(filter_h):
         y_max = y + stride*out_h
@@ -44,10 +44,39 @@ def im2col(input_data, filter_h, filter_w, stride=1, pad=0):
             col[:, :, y, x, :, :] = img[:, :, y:y_max:stride, x:x_max:stride]
 
     col = col.transpose(0, 4, 5, 1, 2, 3).reshape(N*out_h*out_w, -1)
-    print('col.shape : '+str(col.shape))
+    # print('col.shape : '+str(col.shape))
     return col
 
 
+def col2im(col, input_shape, filter_h, filter_w, stride=1, pad=0):
+    """
+
+    Parameters
+    ----------
+    col :
+    input_shape : 入力データの形状（例：(10, 1, 28, 28)）
+    filter_h :
+    filter_w
+    stride
+    pad
+
+    Returns
+    -------
+
+    """
+    N, C, H, W = input_shape
+    out_h = (H + 2*pad - filter_h)//stride + 1
+    out_w = (W + 2*pad - filter_w)//stride + 1
+    col = col.reshape(N, out_h, out_w, C, filter_h, filter_w).transpose(0, 3, 4, 5, 1, 2)
+
+    img = np.zeros((N, C, H + 2*pad + stride - 1, W + 2*pad + stride - 1))
+    for y in range(filter_h):
+        y_max = y + stride*out_h
+        for x in range(filter_w):
+            x_max = x + stride*out_w
+            img[:, :, y:y_max:stride, x:x_max:stride] += col[:, :, y, x, :, :]
+
+    return img[:, :, pad:H + pad, pad:W + pad]
 
 
 
